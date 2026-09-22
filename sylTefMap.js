9d46fec3abac6b74d8d5849cf68306e97910cda2
Kai <kai@wavewrights.com>
2026-09-22 10:23:07 +0100

    Fix date off-by-one

--- sylTefMap.js
+++ sylTefMap.js
@@ -1794,7 +1794,7 @@ function SylTefMap(){
   }
 
   function updatePlayerRoles() {
-    if(Date.now() < new Date("2026-09-25") || Date.now() > new Date("2026-10-05")){
+    if(Date.now() < new Date("2026-09-25") || Date.now() > new Date("2026-10-06")){
       if(!window.location.href.includes("rut")){
         return;
       }
